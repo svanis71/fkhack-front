92486742c6f1507eae44a48156ec522a6338d48c
Johan Svanström <svanis71@gmail.com>
2019-05-14 16:07:07 +0200

    Testar ändra lite

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,53 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 <style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
+:root {
+  --default-background: #2af76e;
+  --default-color: #434343;
+}
 
-#nav
-  padding 30px
-  a
-    font-weight bold
-    color #2c3e50
-    &.router-link-exact-active
-      color #42b983
+html {
+  box-sizing: border-box;
+}
+
+* {
+  margin: 0;
+  box-sizing: inherit;
+}
+
+*:before, *:after {
+  box-sizing: inherit;
+}
+
+body {
+  background: var(--default-background);
+  color: var(--default-color);
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #0000ff;
+
+    &.router-link-exact-active {
+      color: #3333cc;
+    }
+  }
+}
 </style>
