056b34a3256447d028f1ffad8b7cece96d2ef6dd
Rickard Bark <bark.rickard@gmail.com>
2019-05-16 09:45:17 +0200

    läser upp flöde på about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <h1>This is an about page</h1>
+        <p>{{ flode }}</p>
+    </div>
 </template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from 'vue-property-decorator';
+
+    @Component({})
+    export default class Home extends Vue {
+        flode: string = "";
+
+        created() {
+            const data = JSON.parse(sessionStorage.getItem('data') || '{}');
+            this.flode = data.flode;
+        }
+    }
+</script>
